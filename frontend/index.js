cde9abd13b129ca76133da54d24c5211e1da67ee
hardope <adeyeriopeoluwa05@gmail.com>
2024-03-17 12:44:13 +0100

    Random On Load

--- frontend/index.js
+++ frontend/index.js
@@ -4,13 +4,23 @@ function get(selector) {
 }
 
 DOM.addEventListener('DOMContentLoaded', () => {
+    let word = get('word')
+    word.innerHTML = `
+    <h1> ----------********----------</h1>
+    `        
     let request = new XMLHttpRequest()
     request.open('GET', 'https://mydictionary.pythonanywhere.com', true)
     request.onload = function() {
-        let data = JSON.parse(this.response)
+        let data = JSON.parse(request.response)
+        data = data['word'][0]
         if (request.status >= 200 && request.status < 400) {
             let word = get('word')
-            word.innerHTML = `<h2>${data.word.word}</h2>`
+            word.innerHTML = `
+            <h3>${data['word']}</h3>
+            <p>Definition - ${data['definition']}</p>
+            <span>Word Class - ${data['word_class']}</span><br>
+            <span>Length - ${data['length']}</span>
+            `
         } else {
             console.log('error')
         }
