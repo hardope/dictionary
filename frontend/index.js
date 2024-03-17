3eb78d49bbcec9856d26639ad8e798a683622287
hardope <adeyeriopeoluwa05@gmail.com>
2024-03-17 23:54:01 +0100

    Done

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,50 @@ function get(selector) {
   return DOM.getElementById(selector)
 }
 
+function fill(word) {
+    let search_word = get('search')
+    search_word.value = word;
+    search()
+}
+
+function search() {
+    let search = get('search')
+    let word = search.value
+    window.location = `/?=${word}`
+}
+
 DOM.addEventListener('DOMContentLoaded', () => {
+
+    get('search').addEventListener('keyup', (event) => {
+        let search = get('search')
+        let word = search.value
+        let request = new XMLHttpRequest()
+        if (word == '') {
+            let search = get('rec')
+            search.style.display = 'none';
+            return
+        }
+        request.open('GET', `https://mydictionary.pythonanywhere.com/search/${word}`, true)
+        request.onload = function() {
+            if (request.status != 200) {
+                let search = get('rec')
+                search.style.display = 'none';
+                return
+            }
+            let data = JSON.parse(request.response)
+            data = data.slice(0, 10)
+            let words = [...new Set(data.map(i => i['word']))];
+            let search = get('rec')
+            search.style.display = 'block';
+            search.innerHTML = ''
+            for (i = 0; i < words.length; i++) {
+                search.innerHTML += `
+                <div onclick="fill('${words[i]}')">${words[i]}</div>
+                `
+            }
+        }
+        request.send()
+    })
     
     if (window.location.search) {
         let word = window.location.search.split('=')[1]
@@ -24,7 +67,7 @@ DOM.addEventListener('DOMContentLoaded', () => {
 
                 word.innerHTML += `<span>Length - ${data[0]['length']}</span>`                
             } else {
-                console.log('error')
+
                 let word = get('word')
                 word.innerHTML = `<h3>Word not found</h3>`
             }
@@ -46,7 +89,8 @@ DOM.addEventListener('DOMContentLoaded', () => {
                 <span>Length - ${data['length']}</span>
                 `
             } else {
-                console.log('error')
+                let word = get('word')
+                word.innerHTML = `<h3>Word not found</h3>`
             }
         }
         request.send()
